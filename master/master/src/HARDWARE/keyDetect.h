cc9d687c459004b0a6d50032a3a68d29e3c7481d
lihaibin <haibinli@ecarx.com.cn>
2018-09-18 17:25:35 +0800

    first commit

---
+++ master/master/src/HARDWARE/keyDetect.h
@@ -0,0 +1,28 @@
+#ifndef __KEYDETECT_H__
+#define __KEYDETECT_H__
+
+#include <stdbool.h>
+#include "sys.h"
+#include "FreeRTOS.h"
+#include "timers.h"
+#include "rand.h"
+#include "string.h"
+#include "maxtrixApp.h"
+
+typedef enum
+{
+    keyIdx_1,
+    keyIdx_2,
+    keyIdx_3,
+    keyIdx_4,
+    keyIdx_5,
+    keyIdx_6,
+    keyIdx_7,
+    keyIdxNum,
+}keyIndex_t;
+
+
+
+void keyDetectInit(void);
+
+#endif
